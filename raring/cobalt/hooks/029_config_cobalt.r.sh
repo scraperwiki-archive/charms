d2853406ba259bdc1a986a0bcbf4403ad66bf71c
ehg <chris.blower@jumpwire.co.uk>
2013-05-23 12:05:57 +0100

    fix cobalt config; fix node-gyp error

--- raring/cobalt/hooks/029_config_cobalt.r.sh
+++ raring/cobalt/hooks/029_config_cobalt.r.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
+export HOME=/opt/cobalt # https://github.com/TooTallNate/node-gyp/issues/21#issuecomment-17494117
+
 # See http://superuser.com/questions/402794/writing-simple-upstart-script
 cat <<EOF > /etc/init/cobalt.conf
 start on runlevel [2345]
@@ -8,12 +10,12 @@ stop on runlevel [016]
 respawn
 
 script
-    export CU_DB=$(config-get CU_DB)
+    export CU_DB="$(config-get CU_DB)"
     export COBALT_PORT=$(config-get COBALT_PORT)
     export CO_AVOID_BOX_CHECK=$(config-get CO_AVOID_BOX_CHECK)
     export NODE_ENV=$(config-get NODE_ENV)
-    export NODETIME_KEY=$(config-get $NODETIME_KEY)
-    export CO_NODETIME_APP=$(config-get $CO_NODETIME_APP)
+    export NODETIME_KEY=$(config-get NODETIME_KEY)
+    export CO_NODETIME_APP="$(config-get CO_NODETIME_APP)"
     cd /opt/cobalt &&
     . ./activate &&
     echo "Cobalt starting on port or socket $COBALT_PORT" &&
@@ -27,11 +29,12 @@ end script
 
 EOF
 
-# Update what we have
-cd /opt/cobalt
-git pull
 
-. ./activate
+# Update what we have
+cd
+git pull || true
+
+. ./activate || true
 npm install --production 2>&1 || true
 
 # redirect event stderr to /dev/null, as we don't care about errors on stop
