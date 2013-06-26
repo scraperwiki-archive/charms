d2fe12c46e35670be9eaf79c51cb1fc97b1fa848
David Jones <drj@pobox.com>
2013-06-26 16:45:46 +0100

    traceroute typo

--- raring/custard/hooks/020_install_devops_utils.r.sh
+++ raring/custard/hooks/020_install_devops_utils.r.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-apt-get --quiet --quiet --assume-yes traceroute install htop iotop curl strace iftop sysstat time git-core sqlite3 >>/var/log/apt-get.devopsutils.log
+apt-get --quiet --quiet --assume-yes install traceroute htop iotop curl strace iftop sysstat time git-core sqlite3 >>/var/log/apt-get.devopsutils.log
 
 # enable sar
 sed -i 's/ENABLED="false"/ENABLED="true"/;' /etc/default/sysstat
