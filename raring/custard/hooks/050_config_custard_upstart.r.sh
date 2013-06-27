5142fc56acd8df6207e4e5736c180582fb9bfcd3
David Jones <drj@pobox.com>
2013-06-27 16:01:20 +0100

    Move updates of custard so that they run on config-changed.

--- raring/custard/hooks/050_config_custard_upstart.r.sh
+++ raring/custard/hooks/050_config_custard_upstart.r.sh
@@ -45,3 +45,21 @@ post-stop script
 end script
 
 EOF
+
+# When sending email either send a diff (for incremental
+# deploys) or another message (for fresh deploys).
+gitdiff="git diff"
+cd /opt/custard 2>/dev/null || {
+  # Git clone if we haven't found it
+  git clone --quiet git://github.com/scraperwiki/custard.git /opt/custard --depth 1
+  cd /opt/custard
+  gitdiff="echo a fresh deploy"
+}
+git pull --quiet
+
+npm install --production 2>&1 >>/var/log/npm.install.log | egrep -v "^$|^npm http |Checking for |finished successfully|Entering directory|Leaving directory|cxx:|cxx_link:|yes|Nothing to clean|Setting srcdir|Setting blddir" || true
+
+$gitdiff --stat master master@{1} 2>&1 | mail developers@scraperwiki.com -s "Custard has been deployed to $(hostname)"
+
+service custard stop > /dev/null 2>&1
+service custard start
--- raring/custard/hooks/050_setup_custard.r.sh
+++ raring/custard/hooks/050_setup_custard.r.sh
@@ -2,25 +2,7 @@
 export DEBIAN_FRONTEND=noninteractive
 #set -e # should put this back when npm does no errors!
 
-gitdiff="git diff"
-cd /opt/custard 2>/dev/null || {
-  # Git clone if we haven't found it
-  git clone --quiet git://github.com/scraperwiki/custard.git /opt/custard --depth 1
-  cd /opt/custard
-  gitdiff="echo a fresh deploy"
-}
-git pull --quiet
-
 export -p
-npm install --production
-#npm install --production 2>&1 >>/var/log/npm.install.log | egrep -v "^$|^npm http |Checking for |finished successfully|Entering directory|Leaving directory|cxx:|cxx_link:|yes|Nothing to clean|Setting srcdir|Setting blddir" || true
-. ./activate 2>&1 >>/var/log/custard.activate.log | egrep -v "^$|^npm http" || true
 
 # Make tool repo directory
 mkdir -p /opt/tools
-
-# redirect event stderr to /dev/null, as we don't care about errors on stop
-service custard stop >/dev/null 2>&1 || true
-service custard start >/dev/null
-
-$gitdiff --stat master master@{1} 2>&1 | mail developers@scraperwiki.com -s "Custard has been deployed to $(hostname)"
