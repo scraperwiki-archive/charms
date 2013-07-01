100a34af63ef52d58c7819f58e03eb141a0d807b
ehg <chris.blower@jumpwire.co.uk>
2013-07-01 09:20:41 +0100

    fix chroot mail hook

--- raring/cobalt/hooks/070_setup_chroot_mail.r.sh
+++ raring/cobalt/hooks/070_setup_chroot_mail.r.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
-if [ ! -f /opt/basejail/usr/sbin/sendmail ]; then
+if grep -q "Please install" /opt/basejail/usr/sbin/sendmail; then
   cd /usr/local/src
-  git clone git://github.com/hackman/mini_sendmail.git --depth 1
+  git clone -q git://github.com/hackman/mini_sendmail.git --depth 1 || true
   cd mini_sendmail
   make >> /dev/null
   cp mini_sendmail /opt/basejail/usr/sbin/sendmail
