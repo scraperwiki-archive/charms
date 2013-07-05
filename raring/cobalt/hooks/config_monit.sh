686b256e538eba416ec4eacf047209b62c79f4c0
ehg <chris.blower@jumpwire.co.uk>
2013-07-05 11:11:15 +0100

    fix monit config

--- raring/cobalt/hooks/config_monit.sh
+++ raring/cobalt/hooks/config_monit.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 cp -r hooks/config/monit /etc
-monit reload
 sed -i "s:{{HOSTNAME}}:$(hostname):" /etc/monit/conf.d/system
 sed -i "s:{{LOAD_1}}:$(expr $(nproc) '*' 8):" /etc/monit/conf.d/system
 sed -i "s:{{LOAD_5}}:$(expr $(nproc) '*' 6):" /etc/monit/conf.d/system
-sed -i "s:{{LOAD_10}}:$(expr $(nproc) '*' 4):" /etc/monit/conf.d/system
+sed -i "s:{{LOAD_15}}:$(expr $(nproc) '*' 4):" /etc/monit/conf.d/system
+monit reload
