fe996e6caf55067a1843885b46f3533003355955
Francis Irving <francis@flourish.org>
2013-07-06 08:52:24 +0100

    Use noninteractive Debian frontend

--- raring/cobalt/hooks/lib/install_box_software.sh
+++ raring/cobalt/hooks/lib/install_box_software.sh
@@ -12,6 +12,7 @@ export LANGUAGE=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 export LC_CTYPE=en_US.UTF-8
+export DEBIAN_FRONTEND=noninteractive
 
 blue () {
     # Output a blue string.
