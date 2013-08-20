100a9f70a41d2479a655625c9f8f11a77d2fb467
Peter Waller <peter@scraperwiki.com>
2013-08-20 12:33:39 +0100

    Move the tools_rsa chmod

--- raring/custard/hooks/050_config_custard_upstart.r.sh
+++ raring/custard/hooks/050_config_custard_upstart.r.sh
@@ -83,8 +83,8 @@ crontab -l | grep -v deleted-datasets | crontab -
 mkdir -p /etc/custard
 
 touch /etc/custard/tools_rsa
-chmod 0600 /etc/custard/tools_rsa
 config-get TOOLS_RSA_KEY > /etc/custard/tools_rsa
+chmod 0600 /etc/custard/tools_rsa
 
 service custard stop > /dev/null 2>&1
 service custard start
