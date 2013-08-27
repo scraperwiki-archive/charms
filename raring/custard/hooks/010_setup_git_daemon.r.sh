e88f761c8b2209fcdd1086f303a69ee71f3f6063
Peter Waller <peter@scraperwiki.com>
2013-08-27 12:03:32 +0100

    git-daemon should run as custard:custard

--- raring/custard/hooks/010_setup_git_daemon.r.sh
+++ raring/custard/hooks/010_setup_git_daemon.r.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
-INTERNAL_IP=$(/sbin/ifconfig | grep -e 'inet addr:192\.168\.' -e 'inet addr:10\.' | sed 's/.*addr:\([^ ]*\).*/\1/')
-
-mkdir -p /opt/tools
-chown www-data: /opt/tools
-
 cat <<EOF > /etc/init/git-daemon.conf
 start on startup
 stop on runlevel [016]
 respawn
 script
-  /usr/bin/git daemon --listen=${INTERNAL_IP} --syslog --export-all --base-path=/opt/tools --verbose --reuseaddr --user=www-data --group=www-data /opt/tools
+  /usr/bin/git daemon --syslog --export-all --base-path=/opt/tools --verbose --reuseaddr --user=custard --group=custard /opt/tools
 end script
 EOF
+
+restart git-daemon > /dev/null 2>&1 || start git-daemon > /dev/null 2>&1
