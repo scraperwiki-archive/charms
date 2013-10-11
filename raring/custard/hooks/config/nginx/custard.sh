d6793a47b0a6a0c3ae97f5ba063b73ece8b36645
Tom Mortimer-Jones <tom@morty.co.uk>
2013-10-11 14:47:52 +0100

    Added about. contact and journalists pages to list that are forwarded to Wordpress.

--- raring/custard/hooks/config/nginx/custard.sh
+++ raring/custard/hooks/config/nginx/custard.sh
@@ -81,7 +81,7 @@ fi)
       proxy_cache_bypass \$http_x_really_get_it;
       proxy_set_header User-Agent "SCRAPERWIKI-NGINX";
   }
-  location ~ ^/(professional|jobs|tools/tablextract)/?$ {
+  location ~ ^/(professional|jobs|tools/tablextract|about|contact|journalists)/?$ {
       proxy_pass https://blog.scraperwiki.com/\$1/;
       proxy_cache my-cache;
       proxy_cache_valid 5m;
